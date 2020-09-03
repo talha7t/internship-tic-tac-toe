94b284814fc63d2b4f7c5c0f317c47cdc6dd06f9
talha7t <48598366+talha7t@users.noreply.github.com>
2020-09-03 12:07:22 +0500

    going to add PVC functionality

--- scripts/index.js
+++ scripts/index.js
@@ -8,6 +8,8 @@ const playerInfo = document.querySelector(".input-container");
 const gameStart = document.getElementById("start-game");
 const p1Label = document.querySelector(".p1-name");
 const p2Label = document.querySelector(".p2-name");
+const turnMessage = document.querySelector(".turn-message");
+const markContainer = document.querySelector(".mark-container");
 
 const xClass = "x";
 const ticClass = "tic";
@@ -34,60 +36,119 @@ mode.addEventListener("change", modeSelect);
 
 function modeSelect() {
   //function to start game according to mode selected
+  mode.style.display = "none";
   if (mode.value == "pvp") {
     p1Label.style.display = "block";
     p2Label.style.display = "block";
     gameStart.style.display = "block";
 
-    gameStart.addEventListener("click", (e) => {
-      e.preventDefault();
-
+    gameStart.addEventListener("click", () => {
       p1Name = document.querySelector(".player1-name").value;
       p2Name = document.querySelector(".player2-name").value;
 
-      cellElements.forEach((cell) => {
-        cell.classList.add("game-started");
-      });
+      if (p1Name === "" || p2Name === "") {
+        document.querySelector(".input-error").innerText =
+          "Please Enter your Name(s)";
+      } else {
+        turnMessage.innerText = p1Name + `'s turn`;
+        cellElements.forEach((cell) => {
+          cell.classList.add("game-started");
+        });
 
-      startGamePvP();
+        startGamePvP();
+      }
+    });
+  } else if (mode.value == "pvc") {
+    p1Label.style.display = "block";
+    gameStart.style.display = "block";
+    gameStart.addEventListener("click", () => {
+      p1Name = document.querySelector(".player1-name").value;
+
+      if (p1Name === "") {
+        document.querySelector(".input-error").innerText =
+          "Please Enter your Name";
+      } else {
+        startGamePvC();
+      }
+      //  else {
+      //   turnMessage.innerText = p1Name + `'s turn`;
+      //   cellElements.forEach((cell) => {
+      //     cell.classList.add("game-started");
+      //   });
+      // }
     });
   }
 }
 
 function startGamePvP() {
   //function to start pvp starting game
-
+  document.querySelector(".input-error").style.display = "none";
   p1Label.style.display = "none";
   p2Label.style.display = "none";
   gameStart.style.display = "none";
 
   cellElements.forEach((cell) => {
-    // cell.classList.add("game-started");
-    cell.addEventListener("click", clickHandler, { once: true });
+    cell.addEventListener("click", pvpClickHandler, { once: true });
   });
 }
 
-function clickHandler(e) {
+function startGamePvC() {
+  //function to start PVC game
+  document.querySelector(".input-error").style.display = "none";
+  document.querySelector(".mark-select-heading").style.display = "block";
+  p1Label.style.display = "none";
+  gameStart.style.display = "none";
+  markContainer.style.display = "block";
+
+  markContainer.addEventListener("click", (e) => {
+    document.querySelector(".mark-select-heading").style.display = "none";
+    if (e.target.classList.contains("tic-mark")) {
+      markContainer.style.display = "none";
+      console.log("tic selected");
+    } else if (e.target.classList.contains("x-mark")) {
+      markContainer.style.display = "none";
+      console.log("x clicked");
+    }
+  });
+
+  // cellElements.forEach((cell) => {
+  //   // cell.classList.add("game-started");
+  //   cell.addEventListener("click", pvcClickHandler, { once: true });
+  // });
+}
+
+function pvpClickHandler(e) {
   // function to handle click events on the board
 
   const cell = e.target;
   const currentClass = ticTurn ? ticClass : xClass;
+  if (currentClass == "x") {
+    turnMessage.innerText = p1Name + `'s  turn`;
+  } else {
+    turnMessage.innerText = p2Name + `'s  turn`;
+  }
 
   //place mark on the board
   placeMark(cell, currentClass);
 
   //check for win
   if (checkWin(currentClass)) {
-    winMessage.innerText = `${currentClass} wins`;
+    currentClass == xClass
+      ? (winMessage.innerText = p2Name + " wins")
+      : (winMessage.innerText = p1Name + " wins");
 
     cellElements.forEach((cell) => {
-      cell.removeEventListener("click", clickHandler);
+      cell.classList.add("game-finished");
+      cell.removeEventListener("click", pvpClickHandler);
     });
+
     let winRow = winCells(currentClass);
 
     winRow.forEach((index) => {
       cellElements[index].style.backgroundColor = "rgba(20, 240, 20, 0.685)";
     });
+
+    turnMessage.style.display = "none";
   }
 
   //toggle turns
